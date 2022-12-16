81b51e3f924d5e7d4c61a1e473aa96bf531a2c35
Adriana <97173869+a-zajonc@users.noreply.github.com>
2022-12-16 10:36:50 +0100

    Add Comment Component

--- src/components/CommentsSection/CommentBox/index.js
+++ src/components/CommentsSection/CommentBox/index.js
@@ -1,32 +1,15 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CommentBox = void 0;
-const React = __importStar(require("react"));
+const jsx_runtime_1 = require("react/jsx-runtime");
 const react_1 = require("@chakra-ui/react");
+const Rater_1 = require("./Rater");
+const image_amyrobson_png_1 = __importDefault(require("../../../images/avatars/image-amyrobson.png"));
+const icon_reply_svg_1 = __importDefault(require("../../../images/svg/icon-reply.svg"));
 function CommentBox() {
-    return React.createElement(react_1.Box, { fontSize: "100px" }, "Comment");
+    return ((0, jsx_runtime_1.jsxs)(react_1.Box, Object.assign({ display: "flex", flexDirection: "row", bgColor: "white", rounded: "10px", w: "650px", h: "200px", padding: "20px" }, { children: [(0, jsx_runtime_1.jsx)(Rater_1.Rater, {}), (0, jsx_runtime_1.jsxs)(react_1.Box, Object.assign({ display: "flex", flexDirection: "column", marginLeft: "10px" }, { children: [(0, jsx_runtime_1.jsxs)(react_1.Box, Object.assign({ display: "flex", flexDirection: "row", justifyContent: "space-between", w: "100%", marginBottom: "15px" }, { children: [(0, jsx_runtime_1.jsxs)(react_1.Box, Object.assign({ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", w: "50%" }, { children: [(0, jsx_runtime_1.jsx)(react_1.Img, { src: image_amyrobson_png_1.default, alt: "avatar", h: "50px" }), (0, jsx_runtime_1.jsx)(react_1.Text, Object.assign({ color: "black", fontWeight: "500" }, { children: "amyrobson" })), (0, jsx_runtime_1.jsx)(react_1.Text, Object.assign({ color: "#67727E" }, { children: "1 month ago" }))] })), (0, jsx_runtime_1.jsxs)(react_1.Box, Object.assign({ display: "flex", flexDirection: "row", alignItems: "center" }, { children: [(0, jsx_runtime_1.jsx)(react_1.Img, { src: icon_reply_svg_1.default, alt: "reply", marginRight: "10px" }), (0, jsx_runtime_1.jsx)(react_1.Text, Object.assign({ color: "#5457B6", fontWeight: "700" }, { children: "Reply" }))] }))] })), (0, jsx_runtime_1.jsx)(react_1.Text, Object.assign({ color: "#67727E" }, { children: "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well." }))] }))] })));
 }
 exports.CommentBox = CommentBox;
--- src/components/CommentsSection/CommentBox/index.tsx
+++ src/components/CommentsSection/CommentBox/index.tsx
@@ -1,6 +1,55 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Img, Text } from "@chakra-ui/react";
+import { Rater } from "./Rater";
+import avatar from "../../../images/avatars/image-amyrobson.png";
+import imgReply from "../../../images/svg/icon-reply.svg";
 
 export function CommentBox() {
-    return <Box fontSize="100px">Comment</Box>
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      bgColor="white"
+      rounded="10px"
+      w="650px"
+      h="200px"
+      padding="20px"
+    >
+      <Rater />
+      <Box display="flex" flexDirection="column" marginLeft="10px">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          w="100%"
+          marginBottom="15px"
+        >
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            w="50%"
+          >
+            <Img src={avatar} alt="avatar" h="50px"></Img>
+            <Text color="black" fontWeight="500">
+              amyrobson
+            </Text>
+            <Text color="#67727E">1 month ago</Text>
+          </Box>
+          <Box display="flex" flexDirection="row" alignItems="center">
+            <Img src={imgReply} alt="reply" marginRight="10px" />
+            <Text color="#5457B6" fontWeight="700">
+              Reply
+            </Text>
+          </Box>
+        </Box>
+        <Text color="#67727E">
+          Impressive! Though it seems the drag feature could be improved. But
+          overall it looks incredible. You've nailed the design and the
+          responsiveness at various breakpoints works really well.
+        </Text>
+      </Box>
+    </Box>
+  );
 }
