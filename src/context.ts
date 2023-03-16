b92464eb47bb8d30bfcb1f9b0fc6ba01b6656911
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-03-16 13:54:10 +0100

    Remove Unnecessary Imports

--- src/context.ts
+++ src/context.ts
@@ -1,7 +1,3 @@
 import { createContext } from "react";
 
 export const CommentsContext = createContext<any | undefined>([]);
-
-export const ReplyContext = createContext<any | undefined>("");
-
-export const EditContext = createContext<any | undefined>("");
